abda6ec5b856b39f04c40c1a580f93ba54cdd39b
Laura <laurao148@gmail.com>
2018-11-15 23:29:52 +0000

    first commit

---
+++ lib/residencies.rb
@@ -0,0 +1,6 @@
+require "residencies/version"
+
+module Residencies
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/residencies/version.rb
@@ -0,0 +1,3 @@
+module Residencies
+  VERSION = "0.1.0"
+end
